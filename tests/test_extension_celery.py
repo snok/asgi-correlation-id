ed76f4e27afb43fbe9154aa0f7712ff32f8badbb
Sondre Lillebø Gundersen <sondrelg@live.no>
2023-04-22 21:34:25 +0200

    chore: Update pre-commit hooks and lockfile

--- tests/test_extension_celery.py
+++ tests/test_extension_celery.py
@@ -41,7 +41,7 @@ async def test_endpoint_to_worker_to_worker(client, caplog, celery_session_app, 
     """
 
     @default_app.get('/celery-test', status_code=200)
-    async def test_view() -> dict:
+    async def test_view():
         logger.debug('Test view')
         task1.delay().get(timeout=10)
 
