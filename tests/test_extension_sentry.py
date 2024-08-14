38da474facd9296e4260eeaaa55788f03a43ca4f
Sergi Pous <spous@objectvideo.com>
2024-08-14 14:27:39 +0200

    Fix test for version 2.13.0 sentry-sdk

--- tests/test_extension_sentry.py
+++ tests/test_extension_sentry.py
@@ -11,17 +11,25 @@ def test_sentry_sdk_installed(mocker):
     """
     Check that the scope.set_tag method is called when Sentry is installed.
     """
-    set_tag_mock = Mock()
-    scope_mock = Mock()
-    scope_mock.set_tag = set_tag_mock
+    if sentry_sdk.VERSION >= "2.12.0":
+        set_tag_mock = Mock()
+        scope_mock = Mock()
+        scope_mock.set_tag = set_tag_mock
+        mocker.patch.object(sentry_sdk, 'get_isolation_scope', return_value=scope_mock)
+        set_transaction_id(id_value)
+        set_tag_mock.assert_called_once_with('transaction_id', id_value)
+    else:
+        set_tag_mock = Mock()
+        scope_mock = Mock()
+        scope_mock.set_tag = set_tag_mock
 
-    class MockedScope:
-        def __enter__(self):
-            return scope_mock
+        class MockedScope:
+            def __enter__(self):
+                return scope_mock
 
-        def __exit__(self, exc_type, exc_val, exc_tb):
-            pass
+            def __exit__(self, exc_type, exc_val, exc_tb):
+                pass
 
-    mocker.patch.object(sentry_sdk, 'configure_scope', return_value=MockedScope())
-    set_transaction_id(id_value)
-    set_tag_mock.assert_called_once_with('transaction_id', id_value)
+        mocker.patch.object(sentry_sdk, 'configure_scope', return_value=MockedScope())
+        set_transaction_id(id_value)
+        set_tag_mock.assert_called_once_with('transaction_id', id_value)
