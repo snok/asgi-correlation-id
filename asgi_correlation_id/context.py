a9f29e58b5b4efb6f3f442d8b13a65e5613905ba
Sondre Lillebø Gundersen <sondre@favrit.com>
2026-05-31 17:16:50 +0200

    breaking: Drop celery extension

--- asgi_correlation_id/context.py
+++ asgi_correlation_id/context.py
@@ -1,8 +1,3 @@
 from contextvars import ContextVar
 
-# Middleware
 correlation_id: ContextVar[str | None] = ContextVar('correlation_id', default=None)
-
-# Celery extension
-celery_parent_id: ContextVar[str | None] = ContextVar('celery_parent', default=None)
-celery_current_id: ContextVar[str | None] = ContextVar('celery_current', default=None)
