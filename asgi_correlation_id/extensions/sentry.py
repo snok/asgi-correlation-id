e543a49a066f02e63d630569652ea3643b74a1ac
Sondre Lillebø Gundersen <sondre@favrit.com>
2026-06-09 17:05:48 +0200

    fix: Remove version check
    We already require sentry-sdk>=2.12.0, so this check is redundant.

--- asgi_correlation_id/extensions/sentry.py
+++ asgi_correlation_id/extensions/sentry.py
@@ -23,11 +23,6 @@ def set_transaction_id(correlation_id: str) -> None:
     which makes it easier to correlate logs to specific events.
     """
     import sentry_sdk
-    from packaging import version
 
-    if version.parse(sentry_sdk.VERSION) >= version.parse('2.12.0'):
-        scope = sentry_sdk.get_isolation_scope()
-        scope.set_tag('transaction_id', correlation_id)
-    else:
-        with sentry_sdk.configure_scope() as scope:
-            scope.set_tag('transaction_id', correlation_id)
+    scope = sentry_sdk.get_isolation_scope()
+    scope.set_tag('transaction_id', correlation_id)
